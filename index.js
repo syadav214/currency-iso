c618cc1dc984830da2a12850337326fe771bb64f
Santosh Yadav <syadav214@gmail.com>
2020-12-11 17:33:05 +0530

    added currency symbol

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ exports.getISOCode = (code) => getData(code, 'isoCode');
 
 exports.getFractionDigit = (code) => getData(code, 'fractionDigit');
 
+exports.getSymbol = (code) => getData(code, 'symbol');
+
+exports.getUnicode = (code) => getData(code, 'unicode');
+
+exports.getHtmlCode = (code) => getData(code, 'htmlCode');
+
+exports.getHexCode = (code) => getData(code, 'hexCode');
+
 exports.getCurrencyName = (code) => getData(code, 'currencyName');
 
 exports.getCountriesUsingTheCurrency = (code) => getData(code, 'countries');
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const currencyISO = require('./index');
 console.log(currencyISO.getISOCode('EUR'))
 console.log(currencyISO.getFractionDigit('EUR'))
+console.log(currencyISO.getHtmlCode('EUR'))
 console.log(currencyISO.getCurrencyName('EUR'))
-console.log(currencyISO.getCountriesUsingTheCurrency('EUR'))
+//console.log(currencyISO.getCountriesUsingTheCurrency('EUR'))
 //console.log(currencyISO.getFullInformation('EUR'))
 //console.log(currencyISO.getAllCurrencies())
+
+//https://transferwise.com/gb/blog/world-currency-symbols
